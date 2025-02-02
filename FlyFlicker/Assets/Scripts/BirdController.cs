c8124a75333526926c20736e7633b9faf9341fdc
Muhammad Kamran <mkamran.khalid.dev@gmail.com>
2025-02-03 00:12:07 +0500

    Game Update

--- FlyFlicker/Assets/Scripts/BirdController.cs
+++ FlyFlicker/Assets/Scripts/BirdController.cs
@@ -1,7 +1,11 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class BirdController : MonoBehaviour
 {
+    public Rigidbody2D birdRB;
+    public float flapForce;
+    
     void Start()
     {
         
@@ -9,6 +13,11 @@ public class BirdController : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            birdRB.linearVelocity = Vector3.up * flapForce;
+        }
+
+        birdRB.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 }
